0ca20e96c02e3bf57d25500a11379b33046a7ed2
Athan Clark <athan.clark@gmail.com>
2018-03-26 04:05:57 -0600

    using localcooking-shared

--- src/LocalCooking/Database.hs
+++ src/LocalCooking/Database.hs
@@ -1,23 +1,46 @@
 {-# LANGUAGE
-    QuasiQuotes
-  , TemplateHaskell
+    GADTs
+  , QuasiQuotes
   , TypeFamilies
+  , TemplateHaskell
   , MultiParamTypeClasses
-  , GADTs
   , GeneralizedNewtypeDeriving
   #-}
 
 module LocalCooking.Database where
 
+import LocalCooking.Common.Password (HashedPassword)
+import LocalCooking.Common.DeviceToken (DeviceToken)
+import LocalCooking.Common.AuthToken (AuthToken)
+import Facebook.User (FacebookUserAccessToken, FacebookUserId)
 
-import Database.Persist ()
+import Data.Text (Text)
 import Database.Persist.TH (share, persistLowerCase, mkPersist, sqlSettings, mkMigrate)
-
+import Text.EmailAddress (EmailAddress)
 
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+DeviceDescription
+    deviceName Text
+    deviceToken DeviceToken
+    RegisteredDeviceToken deviceToken
+    deriving Show
+
+FacebookUserAccessTokenStored
+    accessToken FacebookUserAccessToken
+    UniqueFacebookUserAccessToken accessToken
+    deriving Show
+
+FacebookUserDetails
+    facebookUserId FacebookUserId
+    facebookSession FacebookUserAccessTokenStoredId []
+    UniqueFacebookUserID facebookUserId
+    deriving Show
+
 User
-    email String
-    password String
+    email EmailAddress
+    password HashedPassword
+    facebookUser FacebookUserDetailsId Maybe
+    UserEmailAddress email
     deriving Show
 |]
