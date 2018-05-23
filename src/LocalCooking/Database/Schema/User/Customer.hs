6f4f92490ed5d0830364cb126bf56b8b32703b04
Athan Clark <athan.clark@gmail.com>
2018-05-23 11:48:55 -0600

    customer details schema

---
+++ src/LocalCooking/Database/Schema/User/Customer.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE
+    GADTs
+  , QuasiQuotes
+  , TypeFamilies
+  , TemplateHaskell
+  , FlexibleInstances
+  , OverloadedStrings
+  , MultiParamTypeClasses
+  , GeneralizedNewtypeDeriving
+  #-}
+
+module LocalCooking.Database.Schema.User.Customer where
+
+import LocalCooking.Database.Schema.User (StoredUserId)
+import LocalCooking.Database.Schema.IngredientDiet (StoredDietId, StoredIngredientId)
+import LocalCooking.Common.User.Name (Name)
+
+import Data.Address (USAAddress)
+import Data.Hashable (Hashable (..))
+import Data.Aeson (ToJSON (..), FromJSON (..), Value (String))
+import Data.Aeson.Types (typeMismatch)
+import Database.Persist.Class (PersistEntity (EntityField, Key))
+import Database.Persist.TH (share, persistLowerCase, mkPersist, sqlSettings, mkMigrate)
+
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+StoredCustomer
+    storedCustomerOwner StoredUserId
+    storedCustomerName Name
+    storedCustomerAddress USAAddress
+    UniqueCustomer storedCustomerOwner
+    deriving Eq Show
+
+StoredDietPreference
+    dietPreferenceOwner StoredCustomerId
+    dietPreferenceDiet  StoredDietId
+    UniqueDietPreference dietPreferenceOwner dietPreferenceDiet
+    deriving Eq Show
+
+StoredAllergy
+    allergyOwner StoredCustomerId
+    allergy      StoredIngredientId
+    UniqueAllergy allergyOwner allergy
+    deriving Eq Show
+|]
