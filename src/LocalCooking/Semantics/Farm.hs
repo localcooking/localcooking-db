d21abc61841f8095aecebe70b5bfffac01d45dec
Athan Clark <athan.clark@gmail.com>
2018-06-20 10:46:30 -0600

    farm shit

---
+++ src/LocalCooking/Semantics/Farm.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE
+    OverloadedStrings
+  , RecordWildCards
+  , DeriveGeneric
+  #-}
+
+module LocalCooking.Semantics.Farm where
+
+
+import Data.Price (Price)
+import Data.Image.Source (ImageSource)
+import Data.Text (Text)
+import Data.Text.Permalink (Permalink)
+import Data.Text.Markdown (MarkdownText)
+import Data.Time (UTCTime)
+import Data.Time.Calendar (Day)
+import Data.Aeson (FromJSON (..), ToJSON (toJSON), Value (Object), (.=), object, (.:))
+import Data.Aeson.Types (typeMismatch)
+import GHC.Generics (Generic)
+import Test.QuickCheck (Arbitrary (..))
+import Test.QuickCheck.Instances ()
