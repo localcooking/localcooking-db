42f60f784745458029892e844ad0edc01f932b54
Athan Clark <athan.clark@gmail.com>
2018-06-22 18:26:10 -0600

    storable, approvable content

--- src/LocalCooking/Database/Schema/Content.hs
+++ src/LocalCooking/Database/Schema/Content.hs
@@ -11,12 +11,36 @@
 
 module LocalCooking.Database.Schema.Content where
 
-import LocalCooking.Common.Tag.Meal (MealTag)
--- import LocalCooking.Database.Schema.Tag.Meal (StoredMealTagId)
+import LocalCooking.Database.Schema.User (StoredUserId)
+import LocalCooking.Database.Schema.User.Editor (StoredEditorId)
+import LocalCooking.Common.ContentRecord (ContentRecord, ContentRecordVariant)
 
-import Data.Image.Source (ImageSource)
+import Data.Time (UTCTime)
 import Database.Persist.TH (share, persistLowerCase, mkPersist, sqlSettings, mkMigrate)
 
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+RecordSubmissionPolicy
+    recordSubmissionPolicyVariant ContentRecordVariant
+    recordSubmissionPolicyAdditional Int
+    UniquePolicyContentRecordVariant recordSubmissionPolicyVariant
+    deriving Eq Show
+
+RecordAssignedSubmissionPolicy
+    recordAssignedSubmissionPolicy RecordSubmissionPolicyId
+    recordAssignedSubmissionPolicyEditor StoredEditorId
+    UniqueAssignedSubmissionPolicy recordAssignedSubmissionPolicy recordAssignedSubmissionPolicyEditor
+    deriving Eq Show
+
+StoredRecordSubmission
+    storedRecordSubmissionAuthor StoredUserId
+    storedRecordSubmissionTimestamp UTCTime
+    storedRecordSubmission ContentRecord
+    deriving Eq Show
+
+RecordSubmissionApproval
+    recordSubmissionApprovalRecord StoredRecordSubmissionId
+    recordSubmissionApprovalEditor StoredEditorId
+    UniqueSubmissionApproval recordSubmissionApprovalRecord recordSubmissionApprovalEditor
+    deriving Eq Show
 |]
