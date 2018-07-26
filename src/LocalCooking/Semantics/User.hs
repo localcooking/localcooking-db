99dc199af8dddaecd74e649124fd844c299d334d
Athan Clark <athan.clark@gmail.com>
2018-07-25 23:18:49 -0600

    user existence

--- src/LocalCooking/Semantics/User.hs
+++ src/LocalCooking/Semantics/User.hs
@@ -5,11 +5,17 @@
   , TemplateHaskell
   #-}
 
-module LocalCooking.Semantics.Role where
+module LocalCooking.Semantics.User where
+
+{-
+
+First-person person perspective via AuthToken
+
+-}
 
 import LocalCooking.Common.User.Role (UserRole)
 
-import Data.Aeson (FromJSON (..), ToJSON (toJSON), Value (Object, Null), (.=), object, (.:))
+import Data.Aeson (FromJSON (..), ToJSON (toJSON), Value (Object, String), (.=), object, (.:))
 import Data.Aeson.Types (typeMismatch)
 import Data.Traversable (traverse)
 import Data.Foldable (asum)
@@ -19,6 +25,29 @@ import Test.QuickCheck (Arbitrary (..))
 import Test.QuickCheck.Gen (oneof)
 import Test.QuickCheck.Instances ()
 
+
+data UserExists a
+  = UserDoesntExist
+  | UserExists a
+  deriving (Eq, Show, Generic)
+
+instance Arbitrary a => Arbitrary (UserExists a) where
+  arbitrary = oneof
+    [ pure UserDoesntExist
+    , UserExists <$> arbitrary
+    ]
+
+instance ToJSON a => ToJSON (UserExists a) where
+  toJSON x = case x of
+    UserDoesntExist -> String "userDoesntExist"
+    UserExists a -> object ["userExists" .= a]
+
+instance FromJSON a => FromJSON (UserExists a) where
+  parseJSON x = case x of
+    String s
+      | s == "userDoesntExist" -> pure UserDoesntExist
+      | otherwise -> typeMismatch "UserExists" x
+    Object o -> UserExists <$> o .: "userExists"
 
 
 data HasRole a
@@ -34,12 +63,14 @@ instance Arbitrary a => Arbitrary (HasRole a) where
 
 instance ToJSON a => ToJSON (HasRole a) where
   toJSON x = case x of
-    DoesntHaveRole -> Null
+    DoesntHaveRole -> String "doesntHaveRole"
     HasRole role a -> object [T.pack (show role) .= a]
 
 instance FromJSON a => FromJSON (HasRole a) where
   parseJSON x = case x of
-    Null -> pure DoesntHaveRole
+    String s
+      | s == "doesntHaveRole" -> pure DoesntHaveRole
+      | otherwise -> typeMismatch "HasRole" x
     Object o -> do
       let getRole role = HasRole role <$> o .: T.pack (show role)
       asum (map getRole [minBound .. maxBound])
